0a054148bf3d00ddb9e9488d7ab44792cf7ed947
SantiiBb <santii_mejia@live.com>
2021-07-28 15:13:27 -0600

    Se añade funcion para paginacion de videos, se eliminan vistas por defecto que genera scaffolding al momento de generar el proyecto, se conecta la base de datos y se generan las capas a utilizar para el proyecto

---
+++ CDIWebSite.Services/Services/VideoRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CDIWebSite.SQL.DataContext;
+
+namespace CDIWebSite.Services.Services
+{
+    class VideoRepository
+    {
+        public List<FnPager_Result> GetVideos(int page)
+        {
+            List<FnPager_Result> lst;
+            using (var db = new CDIWebSiteEntities())
+            {
+                lst = db.FnPager(page, 9).ToList();
+            }
+
+            return lst;
+        }
+    }
+}
